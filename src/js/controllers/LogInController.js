b1f47fcffefe0694a6d40c3beb33ab30ba477d5f
Asier Tamayo <tamasasier@hotmail.com>
2018-02-06 09:59:31 +0100

    First commit. Diseño base

---
+++ src/js/controllers/LogInController.js
@@ -0,0 +1,22 @@
+app.controller('LogInController', function($scope) {
+  $scope.username = "";
+  $scope.password = "";
+  $scope.iniciarSesion = function(){
+    var user = firebase.auth().currentUser;
+    if (user) {
+      // User is signed in.
+      document.getElementById("sesion").innerText = "Sesión iniciada";
+    } else {
+      // No user is signed in.
+      document.getElementById("sesion").innerText = "Sesión no iniciada";
+    }
+    firebase.auth().signInWithEmailAndPassword($scope.username, $scope.password).then(function(){
+      console.log("Sesión iniciada correctamente");
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorCode + errorMessage);
+    });
+  };
+});
